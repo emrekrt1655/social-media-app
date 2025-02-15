6ac661cac0f073b15728f97fead517b8f49c7c79
Emre <emrekurtt1655@gmail.com>
2025-02-16 00:29:30 +0100

    add posts service and query

---
+++ client/src/lib/hooks/usePost.ts
@@ -0,0 +1,64 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  getPosts,
+  getUserPosts,
+  createPost,
+  deletePost,
+} from "../rest/postsService";
+import { PostCreateData, PostsResponse, Post } from "../types/posts";
+
+/**
+ * Fetches all posts
+ */
+
+export function usePosts(userId?: string) {
+    const { data, ...queryResult } = useQuery<PostsResponse>({
+      queryKey: userId ? ["userPosts", userId] : ["posts"], // Dynamically change queryKey
+      queryFn: () => (userId ? getUserPosts(userId) : getPosts()), // Fetch based on userId
+      retry: false,
+    });
+  
+    return {
+      posts: data?.data ? data.data : [] as Post[],
+      ...queryResult,
+    };
+  }
+
+/**
+ * Handles post creation and deletion mutations
+ */
+
+export function usePostMutations(userId: string) {
+  const queryClient = useQueryClient();
+
+  const createPostMutation = useMutation({
+    mutationFn: async (newPost: PostCreateData) => await createPost(newPost),
+    onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["posts"] }); 
+        if (userId) {
+          queryClient.invalidateQueries({ queryKey: ["userPosts", userId] });
+        }
+      },
+    onError: (error) => {
+      console.error("Error creating post:", error);
+    },
+  });
+
+  const deletePostMutation = useMutation({
+    mutationFn: async (postId: string) => await deletePost(postId),
+    onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["posts"] }); 
+        if (userId) {
+          queryClient.invalidateQueries({ queryKey: ["userPosts", userId] });
+        }
+      },
+    onError: (error) => {
+      console.error("Error deleting post:", error);
+    },
+  });
+
+  return {
+    createPost: createPostMutation.mutate,
+    deletePost: deletePostMutation.mutate,
+  };
+}
---
+++ client/src/lib/types/posts.ts
@@ -0,0 +1,32 @@
+export type Post = PostCreateData & {
+  postId: string;
+  category: string;
+  isElite: string;
+  createdAt: string;
+  updatedAt: string;
+  _count: Count;
+};
+
+export type Count = {
+  comments: number;
+  likes: number;
+};
+
+export type PostCreateData = {
+  text: string;
+  postUserId: string;
+  postTopicId: string;
+  image: string;
+};
+
+export type PostCreationResponse = {
+  message: string;
+};
+
+export type PostsResponse = {
+    status: string,
+    message: string,
+    data: Post[]
+}
+
+export type PostDeleteResponse = PostCreationResponse & {};
