a5777cc277392a92e3585f2eaf478d809755ba20
Emre <emrekurtt1655@gmail.com>
2025-05-27 16:08:26 +0200

    tabs on the main page added

--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,11 +1,37 @@
 import "./Main.scss";
+import { usePosts } from "../../lib/hooks/usePost";
+import { Post } from "../../lib/types/posts";
+import { useState } from "react";
 
 const Main = () => {
+  const { posts } = usePosts();
+  const [activeTab, setActiveTab] = useState<"foryou" | "followings">("foryou");
+
   return (
-    <div className="main-container">
-     
-      <div className="main-content">Main Content</div>
-    </div>
+<>
+      <div className="main-toggle">
+        <button
+          className={`toggle-btn ${activeTab === "foryou" ? "active" : ""}`}
+          onClick={() => setActiveTab("foryou")}
+        >
+          For You
+        </button>
+        <button
+          className={`toggle-btn ${activeTab === "followings" ? "active" : ""}`}
+          onClick={() => setActiveTab("followings")}
+        >
+          Followings
+        </button>
+      </div>
+
+      <ul className="post-list">
+        {posts.map((post: Post) => (
+          <li key={post.postId} className="post-item">
+            {post.text}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
